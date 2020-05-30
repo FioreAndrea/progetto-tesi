ab3f401636d991816a76f34cd6aa678607d20b5b
Alf9n5o <a.russo250@studenti.unisa.it>
2020-05-30 14:31:25 +0200

    Added missing datasets

--- constant.py
+++ constant.py
@@ -1,15 +1,18 @@
 from os.path import join
 
-ROOT_DATASET_DIR = r'E:\Unvertità\progettotesi\Dataset'
-ant = join(ROOT_DATASET_DIR, r'ant')
+ROOT_DATASET_DIR = r'C:\Users\alf9n\Documents\GitHub\progetto-tesi\Dataset'
+ant = join(ROOT_DATASET_DIR, 'ant')
+camel = join(ROOT_DATASET_DIR, 'camel')
 ivy = join(ROOT_DATASET_DIR, 'ivy')
 jedit = join(ROOT_DATASET_DIR, 'jedit')
+log4j = join(ROOT_DATASET_DIR, 'log4j')
 lucene = join(ROOT_DATASET_DIR, 'lucene')
 poi = join(ROOT_DATASET_DIR, 'poi')
 synapse = join(ROOT_DATASET_DIR, 'synapse')
 velocity = join(ROOT_DATASET_DIR, 'velocity')
-xalan = join(ROOT_DATASET_DIR, 'xanal')
-xcerces = join(ROOT_DATASET_DIR, 'xcerces')
+xalan = join(ROOT_DATASET_DIR, 'xalan')
+xerces = join(ROOT_DATASET_DIR, 'xerces')
+prop = join(ROOT_DATASET_DIR, 'prop')
 
 
 needed = ['name', 'wmc', 'dit', 'noc', 'cbo', 'rfc', 'lcom',
